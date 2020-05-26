60411d42a867088e678518a5a0653b8bb4b3c07f
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-05-26 11:26:44 -0400

    First step complete...

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
-// Write your JavaScript code here!
+// let pilotName = null;
+// let copilotName = null;
+// let fuelLevel = null;
+// let cargoMass = null;
+
+
+
+window.addEventListener("load", function(){
+   let form = document.querySelector("form");
+   let invalidData = "Invalid data type in submission field: ";
+
+   form.addEventListener("submit", function(event){
+      let pilotName = document.querySelector("input[name=pilotName]").value;
+      let copilotName = document.querySelector("input[name=copilotName]").value;
+      let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+      let cargoMass = document.querySelector("input[name=cargoMass]").value;
+      
+      if (pilotName === "" || copilotName === "" || fuelLevel === "" || cargoMass === ""){
+         alert("Please check your submission. All fields are required.");
+         event.preventDefault();
+      }
+      if (typeof pilotName !== "string"){
+         alert(invalidData + "Pilot Name");
+         event.preventDefault();
+      }
+      if (typeof copilotName !== "string"){
+         alert(invalidData + "Copilot Name");
+         event.preventDefault();
+      }
+      if (isNaN(Number(fuelLevel))){
+         alert(invalidData + "Fuel Level");
+         event.preventDefault();
+      }
+      if (isNaN(Number(cargoMass))){
+         alert(invalidData + "Cargo Mass");
+         event.preventDefault();
+      }
+   });
+});
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>

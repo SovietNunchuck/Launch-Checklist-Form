29aff649b92af9a8e95133c521e735e05ad34874
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-05-28 11:17:57 -0400

    Corrections

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ window.addEventListener("load", function(){
    let form = document.querySelector("form");
    let invalidData = "Invalid data type in submission field: ";
 
+   let json = [];
+      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+         response.json().then(function(json) {
+            const missionTarget = document.getElementById("missionTarget");
+            let x = Math.floor(Math.random() * json.length);
+            missionTarget.innerHTML = `
+            <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[x].name}</li>
+               <li>Diameter: ${json[x].diameter}</li>
+               <li>Star: ${json[x].star}</li>
+               <li>Distance from Earth: ${json[x].distance}</li>
+               <li>Number of Moons: ${json[x].moons}</li>
+            </ol>
+            <img src="${json[x].image}">
+            `;
+         });
+      });
+
    form.addEventListener("submit", function(event){
       let pilotName = document.querySelector("input[name=pilotName]").value;
       let copilotName = document.querySelector("input[name=copilotName]").value;
@@ -11,32 +30,34 @@ window.addEventListener("load", function(){
       let launchStatus = document.getElementById("launchStatus");
       let fuelStatus = document.getElementById("fuelStatus");
       let cargoStatus = document.getElementById("cargoStatus");
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
 
       let ready = true;
+
+      pilotStatus.innerHTML = `Pilot ${pilotName} is ready for launch`;
+      copilotStatus.innerHTML = `Co-pilot ${copilotName} is ready for launch`;
       
       if (pilotName === "" || copilotName === "" || fuelLevel === "" || cargoMass === ""){
          alert("Please check your submission. All fields are required.");
-         return;
       }
-      if (typeof pilotName !== "string"){
+      if (!isNaN(Number(pilotName))){
          alert(`${invalidData}Pilot Name`);
-         return;
+         pilotStatus.innerHTML = "No pilot assigned";
+         ready = false;
       }
-      if (typeof copilotName !== "string"){
+      if (!isNaN(Number(copilotName))){
          alert(`${invalidData}Copilot Name`);
-         return;
+         copilotStatus.innerHTML = "No copilot assigned";
+         ready = false;
       }
       if (isNaN(Number(fuelLevel))){
          alert(`${invalidData}Fuel Level`);
-         return;
+   
       }
       if (isNaN(Number(cargoMass))){
-         alert(`${invalidData}Cargo Mass`);
-         return;
+         alert(`${invalidData}Cargo Mass`);   
       }
-
-      document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName} is ready for launch`;
-      document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilotName} is ready for launch`;
       
 
       if (Number(fuelLevel) >= 0 && Number(fuelLevel) < 10000 && fuelLevel !== "") {
@@ -60,26 +81,6 @@ window.addEventListener("load", function(){
          launchStatus.innerHTML = `Shuttle is ready for launch`;
          launchStatus.style.color = "green";
       }
-
-      let json = [];
-      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-         response.json().then(function(json) {
-            const missionTarget = document.getElementById("missionTarget");
-            let x = Math.floor(Math.random() * json.length);
-            missionTarget.innerHTML = `
-            <h2>Mission Destination</h2>
-            <ol>
-               <li>Name: ${json[x].name}</li>
-               <li>Diameter: ${json[x].diameter}</li>
-               <li>Star: ${json[x].star}</li>
-               <li>Distance from Earth: ${json[x].distance}</li>
-               <li>Number of Moons: ${json[x].moons}</li>
-            </ol>
-            <img src="${json[x].image}">
-            `;
-         });
-      });
-
       event.preventDefault();
    });
 });
